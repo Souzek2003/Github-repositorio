fc72fa800a8f7a335261a7e5bb480784751a1ab8
A708\dam1 <angtispad@alu.edu.gva.es>
2025-10-01 21:19:21 +0200

    Código ratón juguetón

--- programacion_ejercicios/src/main/java/org/example/raton_jugueton.java
+++ programacion_ejercicios/src/main/java/org/example/raton_jugueton.java
@@ -7,5 +7,58 @@ public class raton_jugueton {
     static void main(){
 
         Scanner teclado = new Scanner(System.in);
+
+        final double altura_minima = 140;
+        final double peso_maximo = 120;
+        double altura = 0;
+        double peso = 0;
+
+        System.out.println("***BIENVENIDO AL RATÓN JUGUETÓN***");
+        try {
+            System.out.println("Leyendo altura (cm)... ");
+            altura = teclado.nextDouble();
+        } catch (Exception e) {
+            System.out.println("El formato no es correcto");
+            System.exit(0);
+        }
+
+        if (altura < 0){
+            System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
+        }else{
+            if (altura < altura_minima){
+                System.out.println("LO SIENTO, NO PUEDES MONTAR EN LA ATRACCIÓN. " + "Te faltan " + (altura_minima-altura) + " cm de altura.");
+        }else{
+            if (altura > 230){
+                System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
+            }
+
+            try {
+                System.out.println("Leyendo peso (kg)... ");
+                peso = teclado.nextDouble();
+            } catch (Exception e) {
+                System.out.println("El formato no es correcto");
+                System.exit(0);
+            }
+
+            double peso_minimo = altura*2/8;
+
+            if (peso_minimo < peso){
+                System.out.println("Pesas " + peso + " kg. Peso mínimo calculado: " + peso_minimo + " kg");
+                System.out.println("¡¡SUBE A LA ATRACCIÓN!!");
+            }else{
+                if (peso < 0){
+                    System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
+            }else{
+                if (peso_minimo > peso){
+                    System.out.println("LO SIENTO. Te faltan " + (peso_minimo-peso) + " kg para poder subir a la atracción.");
+                }else{
+                    if (peso > peso_maximo){
+                        System.out.println("LO SIENTO. Sobrepasas " + (peso - peso_maximo) + " kg el límite de la atracción.");
+                    }
+                }
+            }
+            }
+            }
+        }
     }
 }
