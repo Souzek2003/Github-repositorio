7e258ff7b8b63c0404b80c3b5a4283f282a0a0af
Souzek2003 <souzekkfp@gmail.com>
2025-10-03 10:57:42 +0200

    Add files via upload
    Ratón juguetón terminado

--- programacion_ejercicios/src/main/java/org/example/raton_jugueton.java
+++ programacion_ejercicios/src/main/java/org/example/raton_jugueton.java
@@ -25,17 +25,12 @@ public class raton_jugueton {
         }
 
         //Condiciones para la altura. El programa detectará si el usuario pone un número negativo, si le falta altura o si pone una altura superior a 230, dando error en cada caso. Se indicará la altura necesaria para poder subir a la atracción.
-        if (altura < 0){
+        if (altura < 0 || altura > 230) {
             System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
+        }else if (altura < altura_minima) {
+            System.out.println("LO SIENTO, NO PUEDES MONTAR EN LA ATRACCIÓN. " + " Te faltan " + (altura_minima - altura) + " cm de altura.");
         }else{
-            if (altura > 230){
-                System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
-        }else{
-            if (altura < altura_minima){
-                System.out.println("LO SIENTO, NO PUEDES MONTAR EN LA ATRACCIÓN. " + "Te faltan " + (altura_minima-altura) + " cm de altura.");
-            }
-
-            //Try catch para el peso, en caso de que el usuario ponga un formato incorrecto "Hola", dará error y saldra de la consola con exit.
+            //Try catch para el peso, en caso de que el usuario ponga un formato incorrecto "Hola", dará error y saldrá de la consola con exit.
             try {
                 System.out.println("Leyendo peso (kg)... ");
                 peso = teclado.nextDouble();
@@ -44,27 +39,27 @@ public class raton_jugueton {
                 System.exit(0);
             }
 
-            //Cálculo que calcula el peso mínimo que necesitará el usuario para montar a la atracción.
-            double peso_minimo = altura*2/8;
+                //Cálculo que calcula el peso mínimo que necesitará el usuario para montar a la atracción.
+                double peso_minimo = altura*2/8;
 
-            //Condiciones para el peso. El programa detectará si el usuario introduce un número negativo, si le falta peso o si lo sobrepasa, dando error en cada caso. Se indicará el peso necesario para subir a la atracción.
-            if (peso < 0){
-                System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
-            }else{
-                if (peso_minimo > peso){
-                    System.out.println("LO SIENTO. Te faltan " + (peso_minimo-peso) + " kg para poder subir a la atracción.");
-            }else{
-                if (peso > peso_maximo){
-                    System.out.println("LO SIENTO. Sobrepasas " + (peso - peso_maximo) + " kg el límite de la atracción.");
+                //Condiciones para el peso. El programa detectará si el usuario introduce un número negativo, si le falta peso o si lo sobrepasa, dando error en cada caso. Se indicará el peso necesario para subir a la atracción.
+                if (peso < 0){
+                    System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR...");
                 }else{
-                    if (peso_minimo < peso){
-                        System.out.println("Pesas " + peso + " kg. Peso mínimo calculado: " + peso_minimo + " kg");
-                        System.out.println("¡¡SUBE A LA ATRACCIÓN!!");
+                    if (peso_minimo > peso){
+                        System.out.println("LO SIENTO. Te faltan " + (peso_minimo-peso) + " kg para poder subir a la atracción.");
+                    }else{
+                        if (peso > peso_maximo){
+                            System.out.println("LO SIENTO. Sobrepasas " + (peso - peso_maximo) + " kg el límite de la atracción.");
+                        }else{
+                            if (peso_minimo < peso){
+                                System.out.println("Pesas " + peso + " kg. Peso mínimo calculado: " + peso_minimo + " kg");
+                                System.out.println("¡¡SUBE A LA ATRACCIÓN!!");
+                            }
+                        }
                     }
                 }
             }
-            }
-            }
         }
     }
-}
+
