a792f4ffc0c6f878908dac205846b2d853fcf89b
Souzek2003 <souzekkfp@gmail.com>
2025-10-07 12:26:49 +0200

    Add files via upload

--- programacion_ejercicios/src/main/java/org/example/multiplicaciones_3_cifras.java
+++ programacion_ejercicios/src/main/java/org/example/multiplicaciones_3_cifras.java
@@ -8,32 +8,63 @@ public class multiplicaciones_3_cifras {
 
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Introduce el multiplicando (3 cifras): ");
-        int multiplicando = teclado.nextInt();
-        System.out.println("Introduce el multiplicador (3 cifras): ");
-        int multiplicador = teclado.nextInt();
+        int multiplicando = 0;
+        int multiplicador = 0;
 
-        int multiplicandonum = multiplicando; //definimos la variable de tipo numérica con la que vamos a trabajar
-        String numC = Integer.toString(multiplicando); //convertimos el número a carácter
-        String numC_1 = numC.substring(0,1); //cogemos la primera cifra (desde posión 0 hasta 3)
-        int num_1 = Integer.parseInt(numC_1); //convertimos a entero la cifra que hemos cogido, con la que podríamos hacer algún cálculo que necesitemos
+        //Pide los datos al usuario y el programa los lee por teclado
+        do {
+            try {
+                System.out.println("Introduce el multiplicando (3 cifras): ");
+                multiplicando = teclado.nextInt();
 
-        int multiplicadornum = multiplicador; //definimos la variable de tipo numérica con la que vamos a trabajar
-        String numB = Integer.toString(multiplicador); //convertimos el número a carácter
-        String numB_1 = numB.substring(0,1); //cogemos la primera cifra (desde posión 0 hasta 3)
-        int num_2 = Integer.parseInt(numB_1); //convertimos a entero la cifra que hemos cogido, con la que podríamos hacer algún cálculo que necesitemos
+                if (multiplicando < 100 || multiplicando > 999){
+                    System.out.println("Error: Debes introducir un número de 3 cifras (< 100) (> 999).");
+                }
+            } catch (Exception e) {
+                System.out.println("Formato incorrecto");
+                System.exit(0);
+            }
 
-        System.out.println("El producto de la multiplicación es: " + (multiplicandonum*multiplicadornum)); //imprimimos el resultado
+        }while (multiplicando < 100 || multiplicando > 999);
+
+        do {
+            try {
+                System.out.println("Introduce el multiplicador (3 cifras): ");
+                multiplicador = teclado.nextInt();
+
+                if (multiplicador < 100 || multiplicador > 999) {
+                    System.out.println("Error: Debes introducir un número de 3 cifras (< 100) (> 999).");
+                }
+            } catch (Exception e) {
+                System.out.println("Formato incorrecto");
+                System.exit(0);
+            }
+
+            }while (multiplicador < 100 || multiplicador > 999);
+
+        //Convierte el multiplicador a String
+        String multC = Integer.toString(multiplicador);
+
+        //Coge las cifras (del 0 al 1, del 1 al 2 y del 2 al 3)
+        String multC_1 = multC.substring(0, 1);
+        String multC_2 = multC.substring(1, 2);
+        String multC_3 = multC.substring(2, 3);
+
+        //Convierte las cifras a número entero
+        int num1 = Integer.parseInt(multC_1);
+        int num2 = Integer.parseInt(multC_2);
+        int num3 = Integer.parseInt(multC_3);
+
+        //Muestra por pantalla, multiplicando, multiplicador, parciales y resultado final
+        System.out.println("El producto de la multiplicación es: " + (multiplicando*multiplicador));
         System.out.println("El proceso es:");
-        System.out.println("  " + multiplicandonum);
-        System.out.println("x " + multiplicadornum);
+        System.out.println("  " + multiplicando);
+        System.out.println("x " + multiplicador);
         System.out.println("--------");
-        System.out.println("  " + multiplicandonum);
-        System.out.println(" " + multiplicandonum + "x");
-        System.out.println(multiplicandonum + "xx");
+        System.out.println("  " + (multiplicando*num3));
+        System.out.println(" " + (multiplicando*num2) + "x");
+        System.out.println((multiplicando*num1) + "xx");
         System.out.println("--------");
-        System.out.println(multiplicandonum*multiplicadornum);
-
-        //cambio
+        System.out.println(" " + (multiplicando*multiplicador));
     }
 }
