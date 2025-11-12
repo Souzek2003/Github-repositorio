14e86423ed007e03fa21acee870b36584ae70a45
A708\dam1 <angtispad@alu.edu.gva.es>
2025-11-12 21:18:22 +0100

    no message

---
+++ programacion_ejercicios/src/main/java/org/example/ordenar_pivot.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ordenar_pivot {
+
+    static void main() {
+
+        int vector[] = {4,3,2,5,0};
+
+        Arrays.sort(vector);
+
+        System.out.println(Arrays.toString(vector));
+
+
+    }
+}
