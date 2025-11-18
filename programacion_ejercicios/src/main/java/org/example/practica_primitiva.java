a1241c88666571e97aa16ec0509127c525f2a553
Souzek2003 <souzekkfp@gmail.com>
2025-11-18 15:12:08 +0100

    Add files via upload

--- programacion_ejercicios/src/main/java/org/example/practica_primitiva.java
+++ programacion_ejercicios/src/main/java/org/example/practica_primitiva.java
@@ -17,6 +17,8 @@ public class practica_primitiva {
         Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
+        int aciertos=0;
+
 
         System.out.println("Introduce los datos de tu boleto: ");
         String datos = teclado.next();
@@ -33,7 +35,7 @@ public class practica_primitiva {
         int numeros[] = new int[6];
 
         for (int i=0;i<numeros.length;i++){
-            numeros[i] = aleatorio.nextInt(44)+6;
+            numeros[i] = aleatorio.nextInt(49)+1;
         }
 
         boolean comprobar = true;
@@ -43,7 +45,7 @@ public class practica_primitiva {
             Arrays.sort(numeros);
             for (int i=0;i<numeros.length;i++){
                 if (i!= numeros.length-1 && numeros[i]==numeros[i+1]){
-                    numeros[i] = aleatorio.nextInt(44)+6;
+                    numeros[i] = aleatorio.nextInt(49)+1;
                     comprobar = true;
                 }
             }
@@ -54,6 +56,47 @@ public class practica_primitiva {
         System.out.println("Ha salido:");
         System.out.println(Arrays.toString(numeros));
 
+        int numusrint[] = new int[numusr.length];
+        for (int i=0;i<numusr.length;i++){
+            numusrint[i] = Integer.parseInt(numusr[i]);
+        }
+
+        for (int i=0;i<numeros.length;i++){
+            for (int j=0;j<numusrint.length;j++)
+            if (numeros[i]==numusrint[j])
+                aciertos++;
+            }
+
+
+        int numeros2[] = new int[1];
+
+        for (int i=0;i<numeros2.length;i++){
+            numeros2[i] = aleatorio.nextInt(49)+1;
+        }
+
+        System.out.println("");
+        System.out.println("RESULTADOS");
+        System.out.println(aciertos + " aciertos");
+
+
+        int reintegro[] = new int[1];
+        reintegro[0] = aleatorio.nextInt(10);
+
+        if (aciertos == 6) {
+            System.out.println("1ª Categoría: Has acertado 6 números.");
+        } else if (aciertos == 5) {
+            System.out.println("2ª Categoría: Has acertado 5 números.");
+        } else if (aciertos == 5) {
+            System.out.println("3ª Categoría: Has acertado 5 números.");
+        } else if (aciertos == 4) {
+            System.out.println("4ª Categoría: Has acertado 4 números.");
+        } else if (aciertos == 3) {
+            System.out.println("5ª Categoría: Has acertado 3 números.");
+        } else {
+            System.out.println("No premiado.");
+        }
+
+
 
 
 
@@ -62,4 +105,3 @@ public class practica_primitiva {
 
     }
 }
-
